1c0b68717755eee39b85a7be268b2099cb10ef7c
anisimovDanil <danil1998_10@mail.ru>
2021-02-14 23:53:47 +0300

    add_somebody_items

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+//const jwt = require('jsonwebtoken');
 const session = require('express-session'); 
 
 
 const app = express();
+const tokenKey = '1a2b-3c4d-5e6f-7g8h'
 
+// mysql.createConnection
 let connection = mysql.createPool({
 	host: 'localhost',
 	user: 'root',
@@ -18,28 +21,79 @@ const urlencodedParser = bodyParser.urlencoded({extended: false});
 
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
+
+
+
+app.get('/log_in', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/admin', function(req, res){
+	res.sendFile(__dirname + '/admin/admin.html');
+});
+
+app.get('/user', function(req, res){
+	res.sendFile(__dirname + '/user/user.html');
+});
+
+
+
+
+app.use(connect.cookieParser());
+
+app.use(connect.session({ secret: 'your secret here'} ));
+
+/*app.use((req, res, next) => {
+ if(req.headers.authorization){
+   jwt.verify(req.headers.authorization.split(' ')[1], tokenKey, (err, payload) => {
+     if(err)
+       next();
+     else if(payload){
+       for(let user of users){
+         if(user.id === payload.id){
+           req.user = user;
+           next();
+         }
+       }
+
+       if(!req.user) next();
+     }
+   });
+ }
+
+ next();
+});*/
+
+
 
 app.post('/', urlencodedParser, function(req, res) { 
 	var login = req.body.login;
 	var password = req.body.password;
 	if (login && password) {
+		//connection.connect(
 		//connection.query('SELECT * FROM people WHERE login = ? AND password = ?', [login, password], function(error, results, fields) { // protection
 		connection.query("SELECT * FROM people WHERE login = '" + login + "' AND password = '" + password + "'", function(error, results, fields) {
-
 			if (error) throw error;
 			else{
-				if (results.length > 0) {
-					//req.session.loggedin = true;
-					//req.session.login = login;
-					res.end('Helo')
-					///res.redirect('/home');
-				} else {
-					res.send('Incorrect Username and/or Password!');
-				}			
-				res.end();
+				if(results[0].password && results[0].role === 'user'){
+					//res.redirect('/user');
+
+
+					//JWT 
+					/*return res.status(200).json({
+			           id: results[0].id,
+			           login: results[0].login,
+			           token: jwt.sign({id: results[0].id}, tokenKey)
+			         });*/
+
+					req.session.authorized = true;
+					req.session.username = login;
+					
+				}
+
+				if(results[0].password && results[0].role === 'admin'){
+					res.redirect('/admin');
+				}
 			}
 		});
 	} else {
@@ -47,6 +101,8 @@ app.post('/', urlencodedParser, function(req, res) {
 		res.end();
 	}
 });
+
+
 
 
 app.listen(3000, function() {
